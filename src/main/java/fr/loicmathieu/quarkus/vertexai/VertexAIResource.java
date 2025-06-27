b604c18b79248d34cccb6853f06144a52f9e47bd
Loïc Mathieu <loikeseke@gmail.com>
2025-06-27 13:41:48 +0200

    Step 1

---
+++ src/main/java/fr/loicmathieu/quarkus/vertexai/VertexAIResource.java
@@ -0,0 +1,44 @@
+package fr.loicmathieu.quarkus.vertexai;
+
+import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.Content;
+import com.google.cloud.vertexai.api.Part;
+import com.google.cloud.vertexai.generativeai.ContentMaker;
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+
+import java.io.IOException;
+
+// 1. Standard REST endpoint available on /chat
+@Path("/chat")
+public class VertexAIResource {
+    // 2. System message tellin the LLM it is a chatbot
+    private static final String SYSTEM_INSTRUCTION = """
+        You are a chatbot named Loïc. Don't pretend you know everything but try to be helpful with a touch of humour.""";
+
+    private GenerativeModel model;
+
+    // 3. Injection of the provided VertexAI object
+    @Inject
+    VertexAI vertexAI;
+
+    // 4. Init the model with Gemini 2.5 Flash
+    @PostConstruct
+    void initModel() {
+        this.model =  new GenerativeModel("gemini-2.5-flash", vertexAI)
+            .withSystemInstruction(ContentMaker.fromString(SYSTEM_INSTRUCTION));
+    }
+
+    // 5. Generate a chat response on each call to /chat
+    @GET
+    public String chat(@QueryParam("message") String message) throws IOException {
+        var response = model.generateContent(message);
+
+        // For simplicity: we only take the first part of the first candidate
+        return response.getCandidatesList().getFirst().getContent().getPartsList().getFirst().getText() + "\n";
+    }
+}
