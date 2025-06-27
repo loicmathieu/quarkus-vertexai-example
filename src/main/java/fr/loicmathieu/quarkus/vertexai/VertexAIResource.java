a74ab2eac73946190649f8ae995ebcded2964a02
Loïc Mathieu <loikeseke@gmail.com>
2025-06-27 14:40:41 +0200

    Step 2

--- src/main/java/fr/loicmathieu/quarkus/vertexai/VertexAIResource.java
+++ src/main/java/fr/loicmathieu/quarkus/vertexai/VertexAIResource.java
@@ -3,6 +3,7 @@ package fr.loicmathieu.quarkus.vertexai;
 import com.google.cloud.vertexai.VertexAI;
 import com.google.cloud.vertexai.api.Content;
 import com.google.cloud.vertexai.api.Part;
+import com.google.cloud.vertexai.generativeai.ChatSession;
 import com.google.cloud.vertexai.generativeai.ContentMaker;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
 import jakarta.annotation.PostConstruct;
@@ -21,6 +22,7 @@ public class VertexAIResource {
         You are a chatbot named Loïc. Don't pretend you know everything but try to be helpful with a touch of humour.""";
 
     private GenerativeModel model;
+    private ChatSession chatSession;
 
     // 3. Injection of the provided VertexAI object
     @Inject
@@ -31,12 +33,15 @@ public class VertexAIResource {
     void initModel() {
         this.model =  new GenerativeModel("gemini-2.5-flash", vertexAI)
             .withSystemInstruction(ContentMaker.fromString(SYSTEM_INSTRUCTION));
+
+        // For simplicity: we use a single chat session, on real use cases, we should use one per user
+        this.chatSession = this.model.startChat();
     }
 
     // 5. Generate a chat response on each call to /chat
     @GET
     public String chat(@QueryParam("message") String message) throws IOException {
-        var response = model.generateContent(message);
+        var response = chatSession.sendMessage(message);
 
         // For simplicity: we only take the first part of the first candidate
         return response.getCandidatesList().getFirst().getContent().getPartsList().getFirst().getText() + "\n";
